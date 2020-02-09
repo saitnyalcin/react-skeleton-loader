c29990c118b298e8d755ccd00c6c6cfe1e5e55be
steveyalcin <steven.yalcin@gmail.com>
2020-02-08 23:44:52 -0400

    Setingt the constants for the properties successfully

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import './App.css';
 import SkeletonComponent from './SkeletonComponent';
 
 function App() {
+  // set the constants for the properties
+  const title = 'Skeleton Title';
+  const body = 'Skeleton body';
+
   return (
     <div className="App">
-      <SkeletonComponent title="Skeleton title" body="Skeleton body" />
+      <SkeletonComponent title={title} body={body} />
     </div>
   );
 }
