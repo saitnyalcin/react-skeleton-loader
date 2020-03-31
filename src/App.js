9d4b7a6b8e146a109718e6930dafcfd250e9259b
steveyalcin <steven.yalcin@gmail.com>
2020-03-30 21:46:06 -0300

    The skeleton component is moved into the seperate folder called widget

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import SkeletonComponent from './SkeletonComponent';
+import SkeletonComponent from './widgets/SkeletonComponent';
 
 function App() {
   // set the constants for the properties
@@ -9,6 +9,8 @@ function App() {
 
   return (
     <div className="App">
+      <SkeletonComponent title={title} body={body} />
+      <SkeletonComponent title={title} body={body} />
       <SkeletonComponent title={title} body={body} />
     </div>
   );
